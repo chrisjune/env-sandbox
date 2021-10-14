77d84aa6f65ef6ce28968716427908d66ade7ce1
chirsjune <chrisjune@gamil.com>
2021-10-14 20:16:03 +0900

    initial network release

--- main.tf
+++ main.tf
@@ -13,5 +13,18 @@ locals {
 }
 
 # 네트워크구성
+module "aws-network" {
+  source                = "github.com/chrisjune/module-aws-network"
+  env_name              = local.env_name
+  vpc_name              = "msur-VPC"
+  cluster_name          = local.k8s_cluster_name
+  aws_region            = local.aws_region
+  main_vpc_cidr         = "10.10.0.0/16"
+  public_subnet_a_cidr  = "10.10.0.0/18"
+  public_subnet_b_cidr  = "10.10.64.0/18"
+  private_subnet_a_cidr = "10.10.128.0/18"
+  private_subnet_b_cidr = "10.10.192.0/18"
+}
+
 # EKS 구성
 # 깃옵스 구성
